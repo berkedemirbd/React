0c50b3ec6a64e024a9064c673af799d85fcea56d
berkedemirbd <berkedemirbd@hotmail.com>
2024-02-22 21:43:44 +0300

    Edited_2

---
+++ JavaScript/tutorial.js
@@ -0,0 +1,40 @@
+var x="Berke Demir"
+if(x=="Berke Demir"){
+    alert("Şahıs:"+" abla sus biliyosun slay")
+}
+
+else{
+    alert("Defol diye ben")
+}
+
+
+
+if(x=="Berke Demir" || x=="Anonim"){
+    alert("Ne olursan ol yine gel.")
+}
+
+else{
+    alert("Feratlar ve Küpekler giremez.")
+}
+
+
+// Fonksiyon kurabilmek için "prompt" komutu kullanılır.
+prompt()
+
+
+// Konsola 'Hello' yazdrırır
+console.log('Hello')
+
+
+// SelamVer adı altında bir fonksiyon oluşturur ve çağırıldığında ekrana 'Merhaba' yazdırır.
+function selamVer(){
+        console.log("Merhaba")
+}
+
+selamVer()
+
+// Numbers adında bir dizi oluşturur ve bunun 1. elemanını çağırır
+var numbers=[1,3,5,7,9,11,"A","B","C","D","E"]
+numbers[0]
+
+
