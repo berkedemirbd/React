93bb814fcbe80c7c88fb414632101cab50a38f0f
berkedemirbd <berkedemirbd@hotmail.com>
2024-02-23 21:33:57 +0300

    React_2

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
 function App() {
   return (
     <div >
       <h2>Hello From React!!!</h2>
+      <Navi/>
+      <CategoryList/>
+      <ProductList/>
     </div>
   );
 }
---
+++ intro/src/Navi.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Navi extends Component {
+    render(){
+        return(
+            <div>
+                <h2>Navi Component</h2>
+            </div>
+            )
+        }
+}
+
+// Render : Componenti değişen veriye göre revize etmeyi sağlar.
